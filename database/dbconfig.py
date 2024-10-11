f2bd9bd406f5cd21eaf434967e1a9ee600f09063
benar <mulwabenard24@gmial.com>
2024-10-12 00:31:27 +0300

    modified:   database/dbconfig.py

--- database/dbconfig.py
+++ database/dbconfig.py
@@ -1,4 +1,5 @@
 import os
 
 class Config:
+    ##below credentials are local and therefore not a security risk
     DATABASE_URL=os.environ.get('DATABASE_URL') or 'dbname=moviesreservation user=beamer password=allhailkingjulien host=localhost'
