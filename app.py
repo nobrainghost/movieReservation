7181ad415f52a82c596f6f902ccf669a0d6cd239
benar <mulwabenard24@gmial.com>
2024-10-12 17:08:25 +0300

    Added user authentication APIs

--- app.py
+++ app.py
@@ -1,17 +1,76 @@
-from flask import Flask
+from flask import Flask,session
 from flask import render_template,request,make_response,jsonify
+from database.db import create_customer,start_connection,get_user_by_username
+from werkzeug.security import generate_password_hash,check_password_hash
 
 app=Flask(__name__)
-@app.route("/")
-def hello_flask():
-    resp=make_response(render_template('hello.html'))
-    resp.set_cookie('username','johndoe')
-    return resp
+app.secret_key='jhsdvhfvajhbf231354354'
+# @app.route("/")
+# def hello_flask():
+#     resp=make_response(render_template('hello.html'))
+#     resp.set_cookie('username','johndoe')
+#     return resp
 
-@app.route("/a")
-def test__cookie():
-    print(request.cookies.get('username'))
-    username=request.cookies.get('username')
-    return jsonify({"username":username})
+# @app.route("/a")
+# def test__cookie():
+#     print(request.cookies.get('username'))
+#     username=request.cookies.get('username')
+#     return jsonify({"username":username})
+
+@app.route("/api/signup", methods=['GET','POST'])
+def signup():
+    if request.method=='POST':
+        data=request.json
+        username=data['username']
+        password=data['password']
+        email=data['email']
+        conn=start_connection()
+
+        ##Validate if user exists
+
+        create_customer(username,password,email,conn)
+        return("registration Successful")
+    else:
+        return("Error Creating User")
+@app.route("/api/signin",methods=['GET','POST'])    
+def login():
+    if request.method=='POST':
+        data=request.json
+        username=data['username']
+        password=data['password']
+        email=data['email']
+        conn=start_connection()
+
+#later include an get_user_by_email or
+        user=get_user_by_username(username,conn)
+        print(user)
+        if user is not None:
+            customer_id,customer_name,hashed_password=user
+            if check_password_hash(hashed_password,password):
+                print(password)
+                session['customer_id']=customer_id
+                session['customer_name']=customer_name
+                conn.close()
+
+                return jsonify({"message":"Login Succeful"})
+            else:
+                conn.close()
+                return jsonify({"Error": "Invalid Credentials"})
+        else:
+            conn.close()
+            return jsonify({"error":"User not Found"}),404
+            
+@app.route("/api/signout")
+def logout():
+    session.pop('customer_id',None)
+    session.pop('customer_name',None)
+    return jsonify({"Message":"Succeful logout"}),200
+
+       
+
+
+
+
+
 
 
--- database/db.py
+++ database/db.py
@@ -1,6 +1,12 @@
-from dbconfig import Config
-import psycopg2
 
+import psycopg2
+from psycopg2 import sql,Error
+
+import os
+
+class Config:
+    ##below credentials are local and therefore not a security risk
+    DATABASE_URL=os.environ.get('DATABASE_URL') or 'dbname=moviesreservation user=beamer password=allhailkingjulien host=localhost'
 
 def start_connection():
     try:
@@ -57,7 +63,7 @@ start_times TIMESTAMP[]
 """
 
 customer_table_columns="""
-customer_id SERIAL PRIMARY KEY,
+customer_id INTEGER REFERENCES registred_customers_table(customer_id) ON DELETE CASCADE,
 customer_name VARCHAR(100),
 customer_booked_seat INTEGER REFERENCES seats_table(seat_id) ON DELETE CASCADE,
 customer_email VARCHAR(50),
@@ -65,12 +71,65 @@ customer_payment_method VARCHAR(50),
 is_couple BOOLEAN DEFAULT FALSE
 
 """
+registred_customers_columns="""
+customer_id SERIAL PRIMARY KEY,
+customer_username VARCHAR(255),
+customer_password VARCHAR(255),
+customer_email VARCHAR(255)
+"""
 
-def create_base_tables():
-    conn=start_connection()
-    create_table('movie_table',movie_table_columns,conn)
-    create_table('seats_table',seat_table_columns,conn)
-    create_table('customer_table',customer_table_columns,conn)
+# def create_base_tables():
+#     conn=start_connection()
+#     create_table('movie_table',movie_table_columns,conn)
+#     create_table('seats_table',seat_table_columns,conn)
+#     create_table('registred_customers_table',registred_customers_columns,conn)
+#     create_table('customer_table',customer_table_columns,conn)
 
-create_base_tables()
+# create_base_tables()
 
+##move these to a separate file and import the commands
+#basic functions
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+
+
+def create_customer(username,password,email,conn):
+    hashed_password=generate_password_hash(password,method='pbkdf2:sha256')
+    cursor=conn.cursor()
+    query=sql.SQL("INSERT INTO registred_customers_table (customer_username,customer_password,customer_email) VALUES (%s,%s,%s)")
+    cursor.execute(query,[username,hashed_password,email])
+    conn.commit()
+
+# conn=start_connection()
+# create_customer('benar','allhailkingjulien','bean@gmail.com',conn)
+
+def verify_user(username,hashed_password,conn):
+    try:
+        cursor=conn.cursor()
+
+        query='''SELECT customer_id,customer_username,email FROM registred_customers_table WHERE customer_username=? AND password = ?'''
+        cursor.execute(query,(username,hashed_password))
+        user=cursor.fetchone()
+        if user:
+            return {
+                "id": user[0],
+                "username": user[1],
+                "email":user[2]
+            }
+        else:
+            return None
+        
+    except Error as e:
+        print(f"Database error: {e}")
+
+def get_user_by_username(username,conn):
+    cursor=conn.cursor()
+    query="SELECT customer_id,customer_username,customer_password FROM registred_customers_table WHERE customer_username=%s"
+    cursor.execute(query,(username,))
+    user=cursor.fetchone()
+    cursor.close()
+    print(user)
+    return user
+#test the get user
+conn=start_connection()
+get_user_by_username("johndoee",conn)
